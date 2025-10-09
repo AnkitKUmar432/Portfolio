daaf3ca2c42a9feed10ca5422e80373e6bd64736
AnkitKumar432 <ankitkumar27486@gamil.com>
2025-10-09 22:26:52 +0530

    add folder

---
+++ index.js
@@ -0,0 +1,18 @@
+// Toggle Navbar
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+    navLinks.classList.remove("show");
+  });
+});
